48aae5fa6d4982853b32734bec87397b0a1771f2
Cristian Beraha <cristian@cloudops.works>
2026-07-29 12:14:36 -0300

    chore: reinitialize github provider boilerplate from upgraded template
    - Regenerate locals-vars.tf, variables.tf and versions.tf via make init/github
    - Bump cloudopsworks/tags/local module from 1.0.9 to 1.0.10
    - Add descriptions and 3-digit validation to spoke_def, is_hub, org and extra_tags
    - Refresh copyright headers to the 2021-2026 template format
    - Preserve the killmeplz/sodium provider requirement used by main.tf secret encryption
    
    +semver: patch

--- locals-vars.tf
+++ locals-vars.tf
@@ -1,11 +1,15 @@
 ##
-# (c) 2024 - Cloud Ops Works LLC - https://cloudops.works/
-#            On GitHub: https://github.com/cloudopsworks
-#            Distributed Under Apache v2.0 License
+# (c) 2021-2026
+#     Cloud Ops Works LLC - https://cloudops.works/
+#     Find us on:
+#       GitHub: https://github.com/cloudopsworks
+#       WebSite: https://cloudops.works
+#     Distributed Under Apache v2.0 License
 #
+
 module "tags" {
   source  = "cloudopsworks/tags/local"
-  version = "1.0.9"
+  version = "1.0.10"
 
   env = {
     organization_name = var.org.organization_name
